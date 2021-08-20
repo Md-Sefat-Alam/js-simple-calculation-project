cdb78c12bbce7edc3e4f02c4ccc60029c9801836
Md-Sefat-Alam <md.sefatalam@gmail.com>
2021-08-21 03:51:29 +0600

    all added

--- js/main.js
+++ js/main.js
@@ -36,26 +36,67 @@ function buttonControlandSetValue(productType, productTypeCost) {
     else if (productType == "charge") {
         deleveryCharge.innerText = 20;
     }
+    //calling total function for auto update totalPrice
     totalPrice();
+    //calling totalOfTotalPrice for auto update pomo code applayed value
+    totalOfTotalPrice("callFromFunction");
 }
 
 
-function totalPrice() {
+function totalPrice(pomo) {
     //get #extra-memoryPrice price in integer
     const memoryPriceValue = parseInt(document.getElementById("extra-memoryPrice").innerText);
-    console.log(memoryPriceValue);
 
     //get #extra-storageCost price in integer
     const storageCostValue = parseInt(document.getElementById("extra-storageCost").innerText);
-    console.log(storageCostValue);
 
     //get #extra-deleveryCharge price in integer
     const deleveryChargeValue = parseInt(document.getElementById("extra-deleveryCharge").innerText);
-    console.log(deleveryChargeValue)
 
     //get #totalPrice element to set totalPrice value
     const totalPrice = document.getElementById("totalPrice");
 
+    // totalPrice of products selection
     const total = 1299 + memoryPriceValue + storageCostValue + deleveryChargeValue;
+    //set total to #totalPrice
     totalPrice.innerText = total;
+
+    //work for pomo code
+    const pomoMessage = document.getElementById("pomoMessage");
+    const totalOfTotalPrice = document.getElementById("totalOfTotalPrice");
+    if (pomo === true) {
+        const pomoDiscount = (total * 20) / 100;
+        totalOfTotalPrice.innerText = total - pomoDiscount;
+
+        pomoMessage.innerText = "Seccessfully pomo added";
+        pomoMessage.style.color = "green";
+
+    }
+    else if (pomo === false) {
+        totalOfTotalPrice.innerText = total;
+        pomoMessage.innerText = "Don't matched pomo code";
+        pomoMessage.style.color = "red";
+    }
+}
+
+function totalOfTotalPrice(whoCalling) {
+    const pomo = "stevekaku";
+    const getPomoElement = document.getElementById("pomo");
+    const getPomoInput = document.getElementById("pomo").value;
+
+    //using if function for avoid unnecessary result
+    //if call from function
+    //আমি চাইলেই কুপনটি রিসেট করে দিতে পারতাম কিন্তু কুপন দেওয়ার পরেও যেহেতু উপরের বাটনে ক্লিক করে পরিবর্তন করা যাচ্ছে তার জন্য আমি নিচের সিস্টেমটি তৈরি করেছি যাতে কুপন এর মাধ্যমে কম্পানির লোকসান না হয়। [আসা করি বিষয়টি পসিটিভ্লি নিবেন কেননা ভাইয়া বলেছে বাটোনে ক্লিক করলে ভালু রিসেট হয়ে যাবে]
+    if (whoCalling == "callFromFunction" && getPomoInput == "") {
+        return;
+    }
+
+    //if call from button || call from function
+    if (whoCalling == "callFromButton" || whoCalling == "callFromFunction") {
+        if (getPomoInput === pomo) {
+            totalPrice(true);
+        } else {
+            totalPrice(false);
+        }
+    }
 }
